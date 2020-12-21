c04c9c691a0a3c0b66b94a591a8693b0e12b6772
Leanne A Frisinger <67919464+Leaf1204@users.noreply.github.com>
2020-12-20 19:55:26 -0700

    signin working

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -3,26 +3,49 @@ import {GlobalCtx} from "../App";
 
 const Signup = (props) => {
 
-    const {gState, setGState } = React.useContext(GlobalCtx)
-    const {url} = gState
+    const {gState, setGState } = React.useContext(GlobalCtx);
+    const {url} = gState;
 
-    const [form, setForm] = React.useState({
+    const blankForm = {
         username: "",
         password: ""
-    })
+    };
+
+    const [form, setForm] = React.useState(blankForm);
 
     //handle change
      const handleChange = (event) => {
        setForm({...form, [event.target.name]: event.target.value})
      }
 
+     //handle submit 
+
+     const handleSubmit = (event) => {
+         event.preventDefault()
+        const {username, password} = form; 
+
+        fetch(`${url}/auth/signup`, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({username, password}),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            setForm(blankForm);
+            props.history.push("/login");
+        })
+
+     }
 
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type="text"  placeholder="username" name="username" value={form.username} onChange={handleChange}></input>
                 <input type="password" placehold="password" name="password" value={form.password} onChange={handleChange}></input>
-                <input type="sumbit" value="signup"></input>
+                <input type="submit" value="signup" />
             </form>
         </div>
     )
